d9d23527095a0aab333ab49ad9491e6570df576f
niaz-morshed-m <niazmorshed14007@gmail.com>
2025-11-12 00:38:08 +0600

    necessary npm packages are installed and router set up

---
+++ src/Pages/AllCrops.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllCrops = () => {
+    return (
+        <div>
+            it is all crops
+        </div>
+    );
+};
+
+export default AllCrops;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Root/Root.jsx';
+import Home from './Pages/Home.jsx';
+import AllCrops from './Pages/AllCrops.jsx';
+import Login from './Pages/Login.jsx';
+import Register from './Pages/Register.jsx';
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>
+      },
+      {
+        path:'/allCrops',
+        element: <AllCrops></AllCrops>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+    ]
+  },
+]);
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
