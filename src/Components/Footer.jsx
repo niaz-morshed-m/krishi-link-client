bec15060531559cbed0ceb45c2a41d9643aafeb9
niaz-morshed-m <niazmorshed14007@gmail.com>
2025-11-26 01:59:23 +0600

    footercustomised

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from "../assets/Casxsxpture-removebg-preview.png"
+import { FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 const Footer = () => {
     return (
       <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
         <aside>
          <img src={logo} className='w-[140px]' alt="" />
           <p>
-            ACME Industries Ltd.
+           Krishi-Link Inc.
             <br />
-            Providing reliable tech since 1992
+            Providing reliable tech since 2010
           </p>
         </aside>
         <nav>
@@ -18,13 +20,13 @@ const Footer = () => {
           <a className="link link-hover">Marketing</a>
           <a className="link link-hover">Advertisement</a>
         </nav>
-        <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </nav>
+       <nav>
+                 <h6 className="footer-title">Contact Info.</h6>
+                 <a className="link link- text-xl"><FaFacebook></FaFacebook></a>
+                 <a className="link link- text-xl"><FaLinkedin></FaLinkedin></a>
+                 <a className="link link- text-xl"><FaXTwitter></FaXTwitter></a>
+                 <a className="link link-">krishilink@admin.co</a>
+               </nav>
         <nav>
           <h6 className="footer-title">Legal</h6>
           <a className="link link-hover">Terms of use</a>
